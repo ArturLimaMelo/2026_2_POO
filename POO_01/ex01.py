32d08af880bc15f1d4227c31a71e1f6a94a14a03
Gilbert-Silva <gilbertif1995@outlook.com>
2026-09-01 12:32:20 +0000

    ListaPoo01

---
+++ POO_01/ex01.py
@@ -0,0 +1,27 @@
+class Triangulo:
+    def __init__(self):
+        self.b = 0
+        self.h = 0
+    def calcular_area(self):
+        return self.b * self.h / 2
+    
+x = Triangulo()    # Triangulo.__init__(x)  
+print(x.b, x.h) 
+x.b = 10
+x.h = 20
+print(x.b, x.h) 
+print(x.calcular_area()) 
+
+y = Triangulo()
+y.b = 30
+y.h = 40
+print(y.calcular_area()) 
+print(x)
+print(y)
+
+z = x
+z.b = 50
+print(x.b)
+
+
+        
